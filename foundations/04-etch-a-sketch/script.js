a691b33907f06f13c2e9cc97e5d7c703360e8ca1
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-08 15:14:38 +0800

    feat: change hover effect on squares based on mode

--- foundations/04-etch-a-sketch/script.js
+++ foundations/04-etch-a-sketch/script.js
@@ -23,6 +23,7 @@ function makeSquares(number) {
     const square = document.createElement("div");
     square.classList.add("square");
     square.classList.add(i);
+    square.style.backgroundColor = "white";
     square.style.border = "1px solid #d0d0d0";
     square.style.flex = "1 0 auto";
     square.style.aspectRatio = "1 / 1";
@@ -31,14 +32,38 @@ function makeSquares(number) {
     square.style.width = `${squareSide}px`;
     grid.appendChild(square);
 
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "black";
-      square.style.border = "1px solid black";
+    square.addEventListener("mouseenter", () => {
+      if (blackModeButton.classList.contains("active")) {
+        square.style.backgroundColor = "black";
+        square.style.border = "1px solid black";
+        square.style.opacity = "1";
+      } else if (rgbModeButton.classList.contains("active")) {
+        function getRandomColor() {
+          var letters = "0123456789ABCDEF";
+          var color = "#";
+          for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+          }
+          return color;
+        }
+
+        const randomColor = getRandomColor();
+
+        square.style.backgroundColor = `${randomColor}`;
+        square.style.border = `1px solid ${randomColor}`;
+        square.style.opacity = "1";
+      } else if (opacityModeButton.classList.contains("active")) {
+        if (square.style.backgroundColor == "white") {
+          square.style.backgroundColor = "black";
+          square.style.border = "1px solid black";
+          square.style.opacity = +square.style.opacity + 0.1;
+        } else if (square.style.backgroundColor == "black") {
+          square.style.opacity = +square.style.opacity + 0.1;
+        }
+      }
     });
   }
 }
-
-makeSquares(32);
 
 resetButton.addEventListener("click", () => {
   makeSquares(askInput());
@@ -65,3 +90,6 @@ modesContainer.addEventListener("click", (event) => {
       break;
   }
 });
+
+// Initialize a default 64x64 grid on first visit
+makeSquares(64);
