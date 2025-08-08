73304e465395ebdaa77339838df7afd39e3f4e8b
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-08 13:24:39 +0800

    feat: make grid squares based on user input
    Generate grid squares from a user's input retrieved from a prompt that
    also includes error handling for values that are not numbers, negative
    numbers, 0, and greater than 100.

---
+++ foundations/04-etch-a-sketch/script.js
@@ -0,0 +1,26 @@
+const grid = document.querySelector(".grid");
+
+function askInput() {
+  let userInput = +prompt("Input an integer from 1 - 100");
+
+  while (!Number.isInteger(userInput) || userInput <= 0 || userInput > 100) {
+    userInput = +prompt("Invalid input. Please enter an integer from 1 - 100.");
+  }
+
+  return userInput;
+}
+
+function makeSquares(number) {
+  for (let i = 1; i <= number * number; i++) {
+    const square = document.createElement("div");
+    square.classList.add(i);
+    square.style.border = "1px solid #d0d0d0";
+    square.style.flex = "1 0 auto";
+    square.style.aspectRatio = "1 / 1";
+    const squareSide = 760 / number;
+    square.style.width = `${squareSide}px`;
+    grid.appendChild(square);
+  }
+}
+
+makeSquares(askInput());
