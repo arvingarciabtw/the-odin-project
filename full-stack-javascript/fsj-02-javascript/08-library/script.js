8651e35defde388f038f5f6a74b86c563f8211f6
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-14 08:41:51 +0800

    feat: display books on page

--- full-stack-javascript/fsj-02-javascript/08-library/script.js
+++ full-stack-javascript/fsj-02-javascript/08-library/script.js
@@ -1,5 +1,8 @@
 const books = [];
 
+// -- SELECTING DOM ELEMENTS --
+const booksContainer = document.querySelector(".books");
+
 // -- FUNCTIONS --
 function Book(title, author, pages, isRead, id) {
   this.title = title;
@@ -16,6 +19,36 @@ function addBookToBooks(title, author, pages, isRead) {
   return book;
 }
 
+function displayBooks() {
+  for (const book of books) {
+    // Book UI
+    const bookElement = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const isRead = document.createElement("p");
+
+    bookElement.classList.add("book");
+    title.classList.add("title");
+    author.classList.add("author");
+    pages.classList.add("pages");
+    isRead.classList.add("is-read");
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = "A " + book.pages + " page book by";
+    isRead.textContent = book.isRead;
+    bookElement.setAttribute("id", book.id);
+
+    // bookElement.appendChild(isRead);
+    bookElement.appendChild(title);
+    bookElement.appendChild(pages);
+    bookElement.appendChild(author);
+
+    booksContainer.appendChild(bookElement);
+  }
+}
+
 // -- LOG STATEMENTS FOR TESTING --
 const book1 = addBookToBooks("Atomic Habits", "James Clear", 123, true);
 const book2 = addBookToBooks("The Last Days of Socrates", "Plato", 267, false);
@@ -24,3 +57,5 @@ const book3 = addBookToBooks("Circe", "Madeline Miller", 381, false);
 for (const book of books) {
   console.log(book);
 }
+
+displayBooks();
