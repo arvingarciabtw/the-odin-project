d23ec80167dddacefad2c3d3fd11afc27f458bd0
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-14 08:09:04 +0800

    feat: add book to books array

--- full-stack-javascript/fsj-02-javascript/08-library/script.js
+++ full-stack-javascript/fsj-02-javascript/08-library/script.js
@@ -1,1 +1,26 @@
-console.log("Bookmark!");
+const books = [];
+
+// -- FUNCTIONS --
+function Book(title, author, pages, isRead, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.id = id;
+}
+
+function addBookToBooks(title, author, pages, isRead) {
+  const id = self.crypto.randomUUID();
+  const book = new Book(title, author, pages, isRead, id);
+  books.push(book);
+  return book;
+}
+
+// -- LOG STATEMENTS FOR TESTING --
+const book1 = addBookToBooks("Atomic Habits", "James Clear", 123, true);
+const book2 = addBookToBooks("The Last Days of Socrates", "Plato", 267, false);
+const book3 = addBookToBooks("Circe", "Madeline Miller", 381, false);
+
+for (const book of books) {
+  console.log(book);
+}
