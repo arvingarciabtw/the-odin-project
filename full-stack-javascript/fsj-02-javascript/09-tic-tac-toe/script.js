fa2f980a0640b59b6180411c325753837bfee0a4
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-15 11:35:05 +0800

    feat: display game board in console

--- full-stack-javascript/fsj-02-javascript/09-tic-tac-toe/script.js
+++ full-stack-javascript/fsj-02-javascript/09-tic-tac-toe/script.js
@@ -1,72 +1,64 @@
-// You’re going to store the gameboard as an array inside of a Gameboard object, so start there!
-// Your players are also going to be stored in objects, and you’re probably going to want an object to
-// control the flow of the game itself.
-//
-// Your main goal here is to have as little global code as possible. Try tucking as much as you can inside factories.
-// If you only need a single instance of something (e.g. the gameboard, the displayController etc.)
-// then wrap the factory inside an IIFE (module pattern) so it cannot be reused to create additional instances.
-//
-// In this project, think carefully about where each bit of logic should reside.
-// Each little piece of functionality should be able to fit in the game, player or gameboard objects.
-// Take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-//
-// Focus on getting a working game in the console first. Make sure you include logic that checks for when the
-// game is over! You should be checking for all winning 3-in-a-rows and ties. Try to avoid thinking about the DOM
-// and your HTML/CSS until your game is working. Don’t worry about taking user input at this point either.
-// You can call your functions and pass arguments to them to play the game yourself and check if everything is
-// working as intended.
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
 
-// PLAN
-// Game will be in the console FOR NOW
-// TWO user inputs: x (for user 1) and o (for user 2)
-// Output should be a WORKING GAME in the CONSOLE
-//
-// Below are visual representations, but they should be FACTORY FUNCTIONS!!!!!
-// const Gameboard = {
-//    gamebaord: [],
-// }
-//
-// const Player = {
-//    name: string;
-//    score: number;
-// }
-//
-// const Game = {
-//
-// }
-
-// PSEUDOCODE
-// GET USER INPUTS
-// -- Since it will be 3x3, essentially we should get an input 9 times.
-// -- Should alternate between user 1 and user 2 ( x then o, then x...)
-// -- After each input, push it into an array?
-// -- Once we get everything, log the final array
-// -- Maybe log a 3x3 output based from that array
-//
-
-function createPlayer(name, move) {
-  const getName = () => name;
-  const getMove = () => move;
-
-  return { getName, getMove };
-}
-
-const game = (function () {
-  const logMove = (playerName, playerMove) =>
-    playerName + "'s move is: " + playerMove;
-
-  return { logMove };
-})();
-
-const player1 = createPlayer("Batman", "x");
-const player2 = createPlayer("Superman", "o");
-
-const INPUT_AMOUNT = 9;
-
-for (let i = 1; i <= INPUT_AMOUNT; i++) {
-  if (i % 2 !== 0) {
-    console.log(i + ". " + game.logMove(player1.getName(), player1.getMove()));
-  } else {
-    console.log(i + ". " + game.logMove(player2.getName(), player2.getMove()));
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Square());
+    }
   }
+
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    const boardWithSquareValues = board.map((row) =>
+      row.map((square) => square.getValue()),
+    );
+    console.log(boardWithSquareValues);
+  };
+
+  return { getBoard, printBoard };
 }
+
+function Square() {
+  let value = 0;
+
+  // Trigger player's move to change the value of the square
+  const triggerMove = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    triggerMove,
+    getValue,
+  };
+}
+
+const game = (function (playerOneName = "Batman", playerTwoName = "Superman") {
+  const board = Gameboard();
+  const players = [
+    { name: playerOneName, moveType: 1 },
+    { name: playerTwoName, moveType: 2 },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchActivePlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  printNewRound();
+
+  return { getActivePlayer };
+})();
