a3f199724bf7ff601bacfba4a9d46fa9ca6c0f11
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-18 06:24:58 +0800

    build: update deployment configuration

--- full-stack-javascript/fsj-02-javascript/10-restaurant-page/webpack.config.js
+++ full-stack-javascript/fsj-02-javascript/10-restaurant-page/webpack.config.js
@@ -3,20 +3,20 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
+  // devtool: "eval-source-map",
+  // devServer: {
+  //   watchFiles: ["./src/index.html"],
+  // },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: "./src/index.html",
     }),
   ],
   module: {
