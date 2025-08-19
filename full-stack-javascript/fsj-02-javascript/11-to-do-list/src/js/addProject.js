a10cd4c22fbe78ea87b8d82d28cfedeba491680a
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-19 09:08:22 +0800

    feat: add project in sidebar

---
+++ full-stack-javascript/fsj-02-javascript/11-to-do-list/src/js/addProject.js
@@ -0,0 +1,28 @@
+import { allProjects } from "./todoLogic";
+import displayUI from "./displayUI.js";
+import Project from "./projects.js";
+
+const projectsContainer = document.querySelector(".projects-container");
+const addProjectButton = document.querySelector(".btn-add-project");
+const dialogAddProject = document.querySelector(".dialog-add-project")
+const formAddProject = document.querySelector(".form-add-project");
+const inputAddProject = document.querySelector(".input-add-project");
+const closeDialogAddProject = document.querySelector(".btn-close-add-project");
+
+export default function addProject() {
+  addProjectButton.addEventListener("click", () => {
+    dialogAddProject.showModal();
+  })
+
+  closeDialogAddProject.addEventListener("click", () => {
+    dialogAddProject.close();
+  })
+
+  formAddProject.addEventListener("submit", (e) => {
+    e.preventDefault();
+    allProjects.push(new Project(inputAddProject.value));
+    dialogAddProject.close();
+    projectsContainer.innerHTML = "";
+    displayUI();
+  })
+}
