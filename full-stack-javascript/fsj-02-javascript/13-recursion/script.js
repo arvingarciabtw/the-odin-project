be3ef1897dad6cfd5bf89ad1a7cbeba06de1b651
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-22 10:23:24 +0800

    feat: merge sort function

--- full-stack-javascript/fsj-02-javascript/13-recursion/script.js
+++ full-stack-javascript/fsj-02-javascript/13-recursion/script.js
@@ -41,3 +41,55 @@ console.log(`Fibonacci, input 1: ${recursiveFibs(1)}`);
 console.log(`Fibonacci, input 2: ${recursiveFibs(2)}`);
 console.log(`Fibonacci, input 8: ${recursiveFibs(8)}`);
 
+// Recursive merge sort function
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  let sortedLeft = mergeSort(left);
+  let sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
+}
+
+// Helper
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  while (i < left.length) {
+    result.push(left[i]);
+    i++;
+  }
+
+  while (j < right.length) {
+    result.push(right[j]);
+    j++;
+  }
+
+  return result;
+}
+
+console.log("\n -- MERGE SORT -- \n");
+console.log(
+  `Sorted array input of [3, 2, 1, 13, 8, 5, 0, 1]:\n ${mergeSort([3, 2, 1, 13, 8, 5, 0, 1])}`,
+);
+console.log(
+  `Sorted array input of [105, 79, 100, 110]:\n ${mergeSort([105, 79, 100, 110])}`,
+);
