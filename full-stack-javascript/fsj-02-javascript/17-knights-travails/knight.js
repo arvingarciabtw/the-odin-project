b17ce0b370939db516099af87b7156f1739bbe1e
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-23 13:20:39 +0800

    feat: add basic knight movement validation

---
+++ full-stack-javascript/fsj-02-javascript/17-knights-travails/knight.js
@@ -0,0 +1,35 @@
+class Knight {
+  constructor() {
+    this.moves = [
+      [2, 1],
+      [2, -1],
+      [-2, 1],
+      [-2, -1],
+      [1, 2],
+      [1, -2],
+      [-1, 2],
+      [-1, -2],
+    ];
+  }
+
+  isValidPosition(x, y) {
+    return x >= 0 && x < 8 && y >= 0 && y < 8;
+  }
+
+  getValidMoves(position) {
+    const [x, y] = position;
+    const validMoves = [];
+
+    for (const [dx, dy] of this.moves) {
+      const newX = x + dx;
+      const newY = y + dy;
+
+      if (this.isValidPosition(newX, newY)) {
+        validMoves.push([newX, newY]);
+      }
+    }
+
+    return validMoves;
+  }
+
+}
