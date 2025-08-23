2dad2b0ec3c9cc8d31cfdb475027f36ef9b7a80b
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-23 08:24:44 +0800

    feat: implement Node and Tree classes

---
+++ full-stack-javascript/fsj-02-javascript/16-binary-search-trees/bst.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(sortedArray);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) return null;
+
+    const mid = Math.floor(array.length / 2);
+    const node = new Node(array[mid]);
+
+    node.left = this.buildTree(array.slice(0, mid));
+    node.right = this.buildTree(array.slice(mid + 1));
+
+    return node;
+  }
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false,
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+}
