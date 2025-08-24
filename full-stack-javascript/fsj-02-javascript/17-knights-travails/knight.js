37a3b757f8c21eb33f445b0aa4f9a9fe3d815c5b
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-24 09:41:03 +0800

    feat: implements BFS pathfinding algorithm

--- full-stack-javascript/fsj-02-javascript/17-knights-travails/knight.js
+++ full-stack-javascript/fsj-02-javascript/17-knights-travails/knight.js
@@ -40,4 +40,50 @@ class Knight {
     return posStr.split(",").map(Number);
   }
 
+  knightMoves(start, end) {
+    if (
+      !this.isValidPosition(start[0], start[1]) ||
+      !this.isValidPosition(end[0], end[1])
+    ) {
+      throw new Error("Invalid position: coordinates must be between 0 and 7");
+    }
+
+    if (start[0] === end[0] && start[1] === end[1]) {
+      return [start];
+    }
+
+    const queue = [[start]];
+    const visited = new Set();
+    const startStr = this.positionToString(start);
+    const endStr = this.positionToString(end);
+
+    visited.add(startStr);
+
+    while (queue.length > 0) {
+      const currentPath = queue.shift();
+      const currentPosition = currentPath[currentPath.length - 1];
+
+      const validMoves = this.getValidMoves(currentPosition);
+
+      for (const nextPosition of validMoves) {
+        const nextPosStr = this.positionToString(nextPosition);
+
+        if (visited.has(nextPosStr)) {
+          continue;
+        }
+
+        const newPath = [...currentPath, nextPosition];
+
+        if (nextPosStr === endStr) {
+          return newPath;
+        }
+
+        visited.add(nextPosStr);
+        queue.push(newPath);
+      }
+    }
+
+    throw new Error("No path found");
+  }
+
 }
