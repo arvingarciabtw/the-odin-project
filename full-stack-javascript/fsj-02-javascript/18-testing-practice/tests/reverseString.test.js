bd280d31e36bf5de39f87a6de11443f7783af9df
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-25 13:53:01 +0800

    feat: add reverse string function with tests

---
+++ full-stack-javascript/fsj-02-javascript/18-testing-practice/tests/reverseString.test.js
@@ -0,0 +1,17 @@
+const reverseString = require("../scripts/reverseString.js");
+
+test("hello to olleh", () => {
+  expect(reverseString("hello")).toBe("olleh");
+});
+
+test("hello world to dlrow olleh", () => {
+  expect(reverseString("hello world")).toBe("dlrow olleh");
+});
+
+test("hello ! ?1 to 1? ! olleh", () => {
+  expect(reverseString("hello ! ?1")).toBe("1? ! olleh");
+});
+
+test("hEL1o ! ?1 to 1? ! o1LEh", () => {
+  expect(reverseString("hEL1o ! ?1")).toBe("1? ! o1LEh");
+});
