e995254987054fdffb451c786613130e307e83db
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-25 15:04:28 +0800

    feat: implement subtract method with tests

--- full-stack-javascript/fsj-02-javascript/18-testing-practice/scripts/calculator.js
+++ full-stack-javascript/fsj-02-javascript/18-testing-practice/scripts/calculator.js
@@ -7,14 +7,23 @@ class calculator {
   }
 
   static subtract(operandOne, operandTwo) {
+    if (typeof operandOne !== "number" || typeof operandTwo !== "number") {
+      return "ERROR";
+    }
     return operandOne - operandTwo;
   }
 
   static multiply(operandOne, operandTwo) {
+    if (typeof operandOne !== "number" || typeof operandTwo !== "number") {
+      return "ERROR";
+    }
     return operandOne * operandTwo;
   }
 
   static divide(operandOne, operandTwo) {
+    if (typeof operandOne !== "number" || typeof operandTwo !== "number") {
+      return "ERROR";
+    }
     if (operandTwo === 0) {
       return "ERROR";
     }
--- full-stack-javascript/fsj-02-javascript/18-testing-practice/tests/calculator.test.js
+++ full-stack-javascript/fsj-02-javascript/18-testing-practice/tests/calculator.test.js
@@ -17,3 +17,21 @@ describe("addition", () => {
     expect(calculator.add(undefined, null)).toBe("ERROR");
   });
 });
+
+describe("subtraction", () => {
+  test("subtract 1 and 2", () => {
+    expect(calculator.subtract(1, 2)).toBe(-1);
+  });
+  test("subtract 69 and 69", () => {
+    expect(calculator.subtract(69, 69)).toBe(0);
+  });
+  test("an operand is a string", () => {
+    expect(calculator.subtract("2", 5)).toBe("ERROR");
+  });
+  test("an operand is null", () => {
+    expect(calculator.subtract(6, null)).toBe("ERROR");
+  });
+  test("an operand is undefined", () => {
+    expect(calculator.subtract(undefined, null)).toBe("ERROR");
+  });
+});
