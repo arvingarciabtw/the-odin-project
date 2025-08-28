3577200c3cb61358d38b89bc560608b06beea0f9
Arvin Garcia <arvin@arvinagarcia.com>
2025-08-28 10:53:43 +0800

    refactor: dom utilities module

---
+++ full-stack-javascript/fsj-02-javascript/19-battleship/src/scripts/utils/dom-utils.js
@@ -0,0 +1,78 @@
+function coordsToIndex(x, y) {
+  return x * 10 + y;
+}
+
+function indexToCoords(index) {
+  const x = Math.floor(index / 10);
+  const y = index % 10;
+  return [x, y];
+}
+
+function createGrids() {
+  const playerGrid = document.querySelector(".grid-container");
+  const enemyGrid = document.querySelector(".grid-container-enemy");
+
+  for (let i = 0; i < 100; i++) {
+    const cell = document.createElement("button");
+    cell.classList.add("board-cell");
+    cell.classList.add("player-cell");
+    cell.dataset.index = i;
+    playerGrid.appendChild(cell);
+  }
+
+  for (let i = 0; i < 100; i++) {
+    const cell = document.createElement("button");
+    cell.classList.add("board-cell");
+    cell.classList.add("enemy-cell");
+    cell.dataset.index = i;
+    enemyGrid.appendChild(cell);
+  }
+}
+
+function renderGameboard(gameboard, isPlayerBoard = true) {
+  const cells = isPlayerBoard
+    ? document.querySelectorAll(".player-cell")
+    : document.querySelectorAll(".enemy-cell");
+
+  const board = gameboard.renderBoard();
+
+  cells.forEach((cell) => {
+    cell.classList.remove("ship", "hit", "miss", "water");
+  });
+
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      const index = coordsToIndex(x, y);
+      const cell = cells[index];
+      const cellValue = board[x][y];
+
+      switch (cellValue) {
+        case "S":
+          cell.classList.add("ship");
+          break;
+        case "H":
+          cell.classList.add("hit");
+          break;
+        case "X":
+          cell.classList.add("miss");
+          break;
+        case "O":
+          cell.classList.add("water");
+          break;
+      }
+    }
+  }
+}
+
+function renderBothBoards(playerGameboard, enemyGameboard) {
+  renderGameboard(playerGameboard, true);
+  renderGameboard(enemyGameboard, false);
+}
+
+export {
+  coordsToIndex,
+  indexToCoords,
+  createGrids,
+  renderGameboard,
+  renderBothBoards,
+};
