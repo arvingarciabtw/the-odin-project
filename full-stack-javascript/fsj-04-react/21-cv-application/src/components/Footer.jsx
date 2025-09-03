26d9d8eaa13817521c662b24d3cca1d97d50b9c4
Arvin Garcia <arvin@arvinagarcia.com>
2025-09-03 12:00:09 +0800

    style(ui): add footer component

--- full-stack-javascript/fsj-04-react/21-cv-application/src/components/Footer.jsx
+++ full-stack-javascript/fsj-04-react/21-cv-application/src/components/Footer.jsx
@@ -1,7 +1,20 @@
+import "../styles/Footer.css";
+
 function Footer() {
   return (
     <>
-      <h1>Footer</h1>
+      <footer className="wrapper">
+        <p>
+          Made by{" "}
+          <a
+            href="https://github.com/arvingarciabtw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @arvingarciabtw
+          </a>
+        </p>
+      </footer>
     </>
   );
 }
