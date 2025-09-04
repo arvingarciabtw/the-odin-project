d0059bd82ea0d79e63515e6a1dc7c8602722bf22
Arvin Garcia <arvin@arvinagarcia.com>
2025-09-04 11:31:23 +0800

    style(ui): add projects inputs section

--- full-stack-javascript/fsj-04-react/21-cv-application/src/components/App.jsx
+++ full-stack-javascript/fsj-04-react/21-cv-application/src/components/App.jsx
@@ -190,6 +190,17 @@ function App() {
               <Field name="End Date" type="date" placeholder="Jul 2026" />
             </div>
           </Section>
+          <Section heading="Projects">
+            <div className="projects-info-container">
+              <Field
+                name="Project Name"
+                type="text"
+                placeholder="Weather App"
+              />
+              <Field name="Start Date" type="date" placeholder="Jun 2026" />
+              <Field name="End Date" type="date" placeholder="Sep 2026" />
+            </div>
+          </Section>
         </div>
         <Resume />
         <Resume
