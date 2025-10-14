1269cc3ae2b8e4b9a99c9b852b085e93503300b3
Arvin Garcia <arvin@arvingarcia.com>
2025-10-15 07:59:43 +0800

    style(ui): add max-width to base components

--- full-stack-javascript/fsj-04-react/23-shopping-cart/src/components/NavBar.jsx
+++ full-stack-javascript/fsj-04-react/23-shopping-cart/src/components/NavBar.jsx
@@ -4,14 +4,16 @@ import { Link } from 'react-router';
 function NavBar() {
   return (
     <header className={styles.headerApp}>
-      <Link to="/">
-        <h1 className="heading-app">Inkwell</h1>
-      </Link>
-      <nav className={styles.containerNavLinks}>
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
-        <Link to="/cart">Cart</Link>
-      </nav>
+      <div className={styles.wrapper}>
+        <Link to="/">
+          <h1 className="heading-app">Inkwell</h1>
+        </Link>
+        <nav className={styles.containerNavLinks}>
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/cart">Cart</Link>
+        </nav>
+      </div>
     </header>
   );
 }
