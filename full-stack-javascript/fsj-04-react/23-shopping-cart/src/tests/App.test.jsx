bae5a916f6d479082dcef4d3c02602e36dc586a3
Arvin Garcia <arvin@arvingarcia.com>
2025-10-14 19:04:43 +0800

    chore: set up react app with vitest

---
+++ full-stack-javascript/fsj-04-react/23-shopping-cart/src/tests/App.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+
+import App from '../App.jsx';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App title="React" />);
+
+    screen.debug();
+  });
+});
