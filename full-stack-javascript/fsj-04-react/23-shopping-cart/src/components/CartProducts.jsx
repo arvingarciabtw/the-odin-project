c65de9b14c3f1501816083355e1bf96c8b8a6dd2
Arvin Garcia <arvin@arvingarcia.com>
2025-10-16 11:42:18 +0800

    feat: update each cart product's details correctly

--- full-stack-javascript/fsj-04-react/23-shopping-cart/src/components/CartProducts.jsx
+++ full-stack-javascript/fsj-04-react/23-shopping-cart/src/components/CartProducts.jsx
@@ -1,5 +1,60 @@
 import styles from '../styles/CartProducts.module.css';
 
+function CartProduct({ cartProduct, cart }) {
+  function handleClick(type) {
+    if (type === 'increment') {
+      const updatedCartProducts = cart.cartProducts.map((item) =>
+        item.product.id === cartProduct.product.id
+          ? { ...item, count: item.count + 1 }
+          : item,
+      );
+
+      cart.setCartProducts(updatedCartProducts);
+      cart.setCartQuantity(cart.cartQuantity + 1);
+    } else if (type === 'decrement') {
+      const updatedCartProducts = cart.cartProducts.map((item) =>
+        item.product.id === cartProduct.product.id
+          ? { ...item, count: item.count - 1 }
+          : item,
+      );
+
+      cart.setCartProducts(updatedCartProducts);
+      cart.setCartQuantity(cart.cartQuantity - 1);
+    }
+  }
+
+  return (
+    <div key={cartProduct.product.id} className={styles.containerCartProduct}>
+      <img
+        src={cartProduct.product.images[0]}
+        alt={cartProduct.product.title}
+      />
+      <div className={styles.details}>
+        <h3 className={styles.title}>{cartProduct.product.title}</h3>
+        <p className={styles.price}>${cartProduct.product.price}</p>
+      </div>
+      <div className={styles.containerInteractions}>
+        <button
+          className={styles.btnDecrementQuantity}
+          onClick={() => handleClick('decrement')}
+        >
+          -
+        </button>
+        <p className={styles.count}>{cartProduct.count}</p>
+        <button
+          className={styles.btnIncrementQuantity}
+          onClick={() => handleClick('increment')}
+        >
+          +
+        </button>
+      </div>
+      <p className={styles.price}>
+        Total: ${cartProduct.product.price * cartProduct.count}
+      </p>
+    </div>
+  );
+}
+
 function CartProducts({ cart }) {
   return (
     <section className={styles.containerCartProducts}>
@@ -7,27 +62,11 @@ function CartProducts({ cart }) {
         <p>Your cart is empty. Go to our shop and check some goodies out!</p>
       ) : (
         cart.cartProducts.map((cartProduct) => (
-          <div
+          <CartProduct
             key={cartProduct.product.id}
-            className={styles.containerCartProduct}
-          >
-            <img
-              src={cartProduct.product.images[0]}
-              alt={cartProduct.product.title}
-            />
-            <div className={styles.details}>
-              <h3 className={styles.title}>{cartProduct.product.title}</h3>
-              <p className={styles.price}>${cartProduct.product.price}</p>
-            </div>
-            <div className={styles.containerInteractions}>
-              <button className={styles.btnDecrementQuantity}>-</button>
-              <p className={styles.count}>{cartProduct.count}</p>
-              <button className={styles.btnIncrementQuantity}>+</button>
-            </div>
-            <p className={styles.price}>
-              Total: ${cartProduct.product.price * cartProduct.count}
-            </p>
-          </div>
+            cartProduct={cartProduct}
+            cart={cart}
+          />
         ))
       )}
     </section>
