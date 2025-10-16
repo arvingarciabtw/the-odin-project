9a6da8decface81e0187cf3508cea420d0a6b9a9
Arvin Garcia <arvin@arvingarcia.com>
2025-10-16 10:55:58 +0800

    style(ui): style cart products

--- full-stack-javascript/fsj-04-react/23-shopping-cart/src/components/CartProducts.jsx
+++ full-stack-javascript/fsj-04-react/23-shopping-cart/src/components/CartProducts.jsx
@@ -1,18 +1,36 @@
+import styles from '../styles/CartProducts.module.css';
+
 function CartProducts({ cart }) {
   return (
-    <>
+    <section className={styles.containerCartProducts}>
       {cart.cartProducts.length === 0 ? (
         <p>Your cart is empty. Go to our shop and check some goodies out!</p>
       ) : (
         cart.cartProducts.map((cartProduct) => (
-          <div key={cartProduct.product.id}>
-            <p>{cartProduct.product.title}</p>
-            <p>${cartProduct.product.price * cartProduct.count}</p>
-            <p>Count: {cartProduct.count}</p>
+          <div
+            key={cartProduct.product.id}
+            className={styles.containerCartProduct}
+          >
+            <img
+              src={cartProduct.product.images[0]}
+              alt={cartProduct.product.title}
+            />
+            <div className={styles.details}>
+              <h3 className={styles.title}>{cartProduct.product.title}</h3>
+              <p className={styles.price}>${cartProduct.product.price}</p>
+            </div>
+            <div className={styles.containerInteractions}>
+              <button className={styles.btnDecrementQuantity}>-</button>
+              <p className={styles.count}>{cartProduct.count}</p>
+              <button className={styles.btnIncrementQuantity}>+</button>
+            </div>
+            <p className={styles.price}>
+              Total: ${cartProduct.product.price * cartProduct.count}
+            </p>
           </div>
         ))
       )}
-    </>
+    </section>
   );
 }
 
