79530eb3fcf9f8c6d4eaca744acb33ceac13cecd
Arvin Garcia <arvin@arvingarcia.com>
2025-10-26 12:53:57 +0800

    feat: navigation for platform CRUD pages

--- full-stack-javascript/fsj-06-nodejs/27-inventory-application/routes/platformsRouter.js
+++ full-stack-javascript/fsj-06-nodejs/27-inventory-application/routes/platformsRouter.js
@@ -2,6 +2,18 @@ const { Router } = require('express');
 const platformsRouter = Router();
 const { getGamesByPlatform } = require('../controllers/gamesController');
 
+platformsRouter.get('/create', (_req, res) => {
+  res.render('./platforms/create');
+});
+
+platformsRouter.get('/update', (_req, res) => {
+  res.render('./platforms/update');
+});
+
+platformsRouter.get('/delete', (_req, res) => {
+  res.render('./platforms/delete');
+});
+
 platformsRouter.get('/:name', getGamesByPlatform);
 
 module.exports = platformsRouter;
