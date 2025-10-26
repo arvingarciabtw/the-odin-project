fcb7001dfcf615170a5fb067f290e5c1f704b7a0
Arvin Garcia <arvin@arvingarcia.com>
2025-10-26 08:52:25 +0800

    chore: update eslint config

--- full-stack-javascript/fsj-06-nodejs/27-inventory-application/eslint.config.mjs
+++ full-stack-javascript/fsj-06-nodejs/27-inventory-application/eslint.config.mjs
@@ -1,18 +1,22 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
+export default [
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-  },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  {
-    env: {
-      node: true,
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
-]);
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+];
