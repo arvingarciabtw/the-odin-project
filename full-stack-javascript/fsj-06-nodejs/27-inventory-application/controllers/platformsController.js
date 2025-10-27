c84473248ade38cabfeed1d195308a27f6abcdd2
Arvin Garcia <arvin@arvingarcia.com>
2025-10-27 14:47:31 +0800

    feat: add input validation for create platform

--- full-stack-javascript/fsj-06-nodejs/27-inventory-application/controllers/platformsController.js
+++ full-stack-javascript/fsj-06-nodejs/27-inventory-application/controllers/platformsController.js
@@ -1,3 +1,18 @@
+const { body, validationResult, matchedData } = require('express-validator');
+
+// == VALIDATION AND SANITIZATION ==
+
+const validatePlatform = [
+  body('createPlatform')
+    .trim()
+    .matches(/^[a-zA-Z0-9\s]+$/)
+    .withMessage('The field must only have letters and numbers.')
+    .notEmpty()
+    .withMessage('The field must not be empty.'),
+];
+
+// == GET REQUESTS ==
+
 function createPlatformGet(_req, res) {
   res.render('./platforms/create');
 }
@@ -10,8 +25,27 @@ function deletePlatformGet(_req, res) {
   res.render('./platforms/delete');
 }
 
+// == POST REQUESTS ==
+
+const createPlatformPost = [
+  validatePlatform,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render('./platforms/create', {
+        errors: errors.array(),
+      });
+    }
+
+    const { createPlatform } = matchedData(req);
+    // otherwise, then have some sql stuff to add the input to the database
+    res.redirect('/');
+  },
+];
+
 module.exports = {
   createPlatformGet,
   updatePlatformGet,
   deletePlatformGet,
+  createPlatformPost,
 };
--- full-stack-javascript/fsj-06-nodejs/27-inventory-application/routes/platformsRouter.js
+++ full-stack-javascript/fsj-06-nodejs/27-inventory-application/routes/platformsRouter.js
@@ -4,6 +4,7 @@ const { getGamesByPlatform } = require('../controllers/gamesController');
 const platformsController = require('../controllers/platformsController');
 
 platformsRouter.get('/create', platformsController.createPlatformGet);
+platformsRouter.post('/create', platformsController.createPlatformPost);
 platformsRouter.get('/update', platformsController.updatePlatformGet);
 platformsRouter.get('/delete', platformsController.deletePlatformGet);
 
