5d47b77ec1c002580815881f916b5144e50d0df6
Arvin Garcia <arvin@arvingarcia.com>
2025-10-29 14:55:12 +0800

    feat: implement local auth and hashed password

--- full-stack-javascript/fsj-06-nodejs/28-members-only/app.js
+++ full-stack-javascript/fsj-06-nodejs/28-members-only/app.js
@@ -7,6 +7,8 @@ const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const { pool } = require('./models/pool');
+const indexRouter = require('./routes/indexRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,6 +22,52 @@ app.use(
 );
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+
+// == PASSPORT ==
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        'SELECT * FROM users WHERE username = $1',
+        [username],
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+
+      const match = bcrypt.compare(user.password, password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+// == ROUTES ==
+
+app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3000;
 
---
+++ full-stack-javascript/fsj-06-nodejs/28-members-only/routes/indexRouter.js
@@ -0,0 +1,48 @@
+const { Router } = require('express');
+const indexRouter = Router();
+const { pool } = require('../models/pool');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+indexRouter.get('/', (req, res) => {
+  res.render('index', { user: req.user });
+});
+indexRouter.get('/sign-up', (req, res) => {
+  res.render('sign-up-form');
+});
+indexRouter.post('/sign-up', async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await pool.query(
+      'INSERT INTO users (first_name, last_name, username, password, membership_status) VALUES ($1, $2, $3, $4, $5)',
+      [
+        req.body.firstName,
+        req.body.lastName,
+        req.body.username,
+        hashedPassword,
+        'false',
+      ],
+    );
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+indexRouter.post(
+  '/log-in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+  }),
+);
+indexRouter.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+module.exports = indexRouter;
