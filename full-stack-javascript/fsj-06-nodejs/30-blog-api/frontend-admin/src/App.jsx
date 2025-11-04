ef98619c4a088d0840c4f75eaa3f3c9e60504f25
Arvin Garcia <arvin@arvingarcia.com>
2025-11-04 08:08:53 +0800

    chore: set up project

---
+++ full-stack-javascript/fsj-06-nodejs/30-blog-api/frontend-admin/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>At frontend admin.</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ full-stack-javascript/fsj-06-nodejs/30-blog-api/frontend-user/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>At frontend user.</h1>
+    </>
+  );
+}
+
+export default App;
