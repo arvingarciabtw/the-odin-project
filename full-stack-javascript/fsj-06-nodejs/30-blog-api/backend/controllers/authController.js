2836cf99c208664520a51cfadfb8d17eaad45521
Arvin Garcia <arvin@arvingarcia.com>
2025-11-04 13:17:34 +0800

    refactor: move callbacks into controllers

---
+++ full-stack-javascript/fsj-06-nodejs/30-blog-api/backend/controllers/authController.js
@@ -0,0 +1,68 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '../generated/prisma/client.js';
+
+const prisma = new PrismaClient();
+
+async function postRegister(req, res) {
+  const { firstName, lastName, username, password } = req.body;
+
+  try {
+    let user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+    if (user) return res.status(400).json({ msg: 'User already exists' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    res.json({ msg: 'User created successfully.' });
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error.', error: err.message });
+  }
+}
+
+async function postLogin(req, res) {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+    if (!user) return res.status(400).json({ msg: 'User not found' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials.' });
+
+    const payload = { id: user.id, username: user.username };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' },
+      (err, token) => {
+        if (err) throw new Error(err);
+        res.json({ token });
+      },
+    );
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error.' });
+  }
+}
+
+export default {
+  postRegister,
+  postLogin,
+};
--- full-stack-javascript/fsj-06-nodejs/30-blog-api/backend/routes/auth.js
+++ full-stack-javascript/fsj-06-nodejs/30-blog-api/backend/routes/auth.js
@@ -1,70 +1,13 @@
 import 'dotenv/config';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import passport from 'passport';
+import authController from '../controllers/authController.js';
 import { Router } from 'express';
-import { PrismaClient } from '../generated/prisma/client.js';
 
 const authRouter = Router();
-const prisma = new PrismaClient();
 
-authRouter.post('/register', async (req, res) => {
-  const { firstName, lastName, username, password } = req.body;
+authRouter.post('/register', authController.postRegister);
 
-  try {
-    let user = await prisma.user.findUnique({
-      where: {
-        username: username,
-      },
-    });
-    if (user) return res.status(400).json({ msg: 'User already exists' });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        username,
-        password: hashedPassword,
-      },
-    });
-
-    res.json({ msg: 'User created successfully.' });
-  } catch (err) {
-    res.status(500).json({ msg: 'Server error.', error: err.message });
-  }
-});
-
-authRouter.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        username,
-      },
-    });
-    if (!user) return res.status(400).json({ msg: 'User not found' });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials.' });
-
-    const payload = { id: user.id, username: user.username };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' },
-      (err, token) => {
-        if (err) throw new Error(err);
-        res.json({ token });
-      },
-    );
-  } catch (err) {
-    res.status(500).json({ msg: 'Server error.' });
-  }
-});
+authRouter.post('/login', authController.postLogin);
 
 authRouter.get(
   '/profile',
