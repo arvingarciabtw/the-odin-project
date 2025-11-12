f68f5276aa6c5b472558ce64af5d092b15296904
Arvin Garcia <arvin@arvingarcia.com>
2025-11-12 15:12:52 +0800

    feat: add a red box based on click coordinates

--- full-stack-javascript/fsj-06-nodejs/31-wheres-waldo/frontend/src/pages/Game.jsx
+++ full-stack-javascript/fsj-06-nodejs/31-wheres-waldo/frontend/src/pages/Game.jsx
@@ -1,9 +1,21 @@
-import { Link } from 'react-router-dom';
+import styles from '../styles/Game.module.css';
 import Description from '../components/Description';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Game() {
+  const [boxStyle, setBoxStyle] = useState({});
+
+  function handleClick(e) {
+    setBoxStyle({
+      borderColor: 'red',
+      left: `${e.pageX - 20}px`,
+      top: `${e.pageY - 20}px`,
+    });
+  }
+
   return (
-    <main>
+    <main className={styles.game}>
       <Description
         title="Catch them!"
         description="Find the three Pokémon indicated. Go on and catch them!"
@@ -11,6 +23,13 @@ function Game() {
       <Link to="/">
         <button>Give Up</button>
       </Link>
+      <div className={styles.redBox} style={boxStyle}></div>
+      <img
+        src="/pokemon-wheres-waldo.webp"
+        alt="Pokemon Where's Waldo version"
+        className={styles.imgPokemon}
+        onClick={handleClick}
+      />
     </main>
   );
 }
