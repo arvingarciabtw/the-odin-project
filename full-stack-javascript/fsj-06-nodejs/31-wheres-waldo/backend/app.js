90b8b6c28774f77175387960f727711f0c8670a8
Arvin Garcia <arvin@arvingarcia.com>
2025-11-13 14:36:03 +0800

    chore: set up backend

---
+++ full-stack-javascript/fsj-06-nodejs/31-wheres-waldo/backend/app.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send("Backend for Where's Waldo running!");
+});
+
+app.listen(process.env.PORT, (err) => {
+  if (err) throw new Error(err);
+
+  console.log('Listening on port', process.env.PORT);
+});
