0df8ac83d6af9351067059fb6c41bc32a5a50b25
Arvin Garcia <arvin@arvingarcia.com>
2025-11-19 12:02:27 +0800

    feat: implement auth in backend

--- full-stack-javascript/fsj-06-nodejs/33-odin-book/backend/app.js
+++ full-stack-javascript/fsj-06-nodejs/33-odin-book/backend/app.js
@@ -1,7 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import passport from "passport";
+import passportConfig from "./passport.js";
+import routes from "./routes/routes.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(passport.initialize());
+passportConfig(passport);
+
+app.use("/api/auth", routes.auth);
 
 app.get("/", (_req, res) => {
 	res.send("Backend for Yappr running!");
