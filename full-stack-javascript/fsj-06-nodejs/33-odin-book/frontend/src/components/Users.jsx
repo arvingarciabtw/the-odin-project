ef736ed71a5ff704476e7886f571bcc3eba3f270
Arvin Garcia <arvin@arvingarcia.com>
2025-11-22 07:00:05 +0800

    feat: display users in frontend

--- full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/Users.jsx
+++ full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/Users.jsx
@@ -1,36 +1,80 @@
 import styles from "../styles/Users.module.css";
+import { useState, useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { api } from "../utils/api";
 
 function Users() {
+	const { user: loggedUser } = useAuth();
+
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		try {
+			async function fetchUsers() {
+				const response = await api.get("/api/users");
+				const users = await response.json();
+
+				const filteredUsers = users.filter((user) => user.id !== loggedUser.id);
+
+				setUsers(filteredUsers);
+			}
+
+			fetchUsers();
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}, []);
+
 	return (
 		<section className={styles.users}>
-			<User />
-			<User />
-			<User />
-			<User />
-			<User />
+			{users.map((user) => (
+				<User key={user.id} user={user} />
+			))}
 		</section>
 	);
 }
 
-function User() {
+function User({ user }) {
+	const { user: loggedUser } = useAuth();
+
+	const isFollowing = user.following.some(
+		(item) => item.follower_id === loggedUser.id && item.status === "ACCEPTED",
+	);
+
+	const isPending = user.following.some(
+		(item) => item.follower_id === loggedUser.id && item.status === "PENDING",
+	);
+
+	let status;
+
+	if (isFollowing) {
+		status = "Unfollow";
+	} else if (isPending) {
+		status = "Pending";
+	} else {
+		status = "Follow";
+	}
+
 	return (
 		<div className={styles.user}>
 			<img src="/logo_optimized.webp" alt="Profile picture" />
 			<div className={styles.userDetails}>
-				<p className={styles.fullname}>First Last</p>
+				<p className={styles.fullname}>
+					{user.first_name} {user.last_name}
+				</p>
 				<div className={styles.bottom}>
-					<p>@username</p>
+					<p>@{user.username}</p>
 					<div className={styles.separator}></div>
 					<p>
-						<span>10</span> followers
+						<span>{user.following.length}</span> followers
 					</p>
 					<div className={styles.separator}></div>
 					<p>
-						<span>10</span> following
+						<span>{user.followers.length}</span> following
 					</p>
 				</div>
 			</div>
-			<button className={styles.followButton}>Follow</button>
+			<button className={styles.followButton}>{status}</button>
 		</div>
 	);
 }
