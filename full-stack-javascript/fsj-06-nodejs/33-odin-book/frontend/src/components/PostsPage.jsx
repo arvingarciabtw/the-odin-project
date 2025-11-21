0b35d01311fee7c6fbfdd9e1747fd405c5f3ba7a
Arvin Garcia <arvin@arvingarcia.com>
2025-11-22 07:53:08 +0800

    feat: display correct posts in posts index page

--- full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/PostsPage.jsx
+++ full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/PostsPage.jsx
@@ -1,15 +1,150 @@
 import styles from "../styles/PostsPage.module.css";
 import Hamburger from "../components/Hamburger";
 import Sidebar from "../components/Sidebar";
-import Posts from "../components/Posts";
+import Post from "../components/Post";
+import { useState, useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { formatDistanceToNow } from "date-fns";
+import { api } from "../utils/api";
 
 function PostsPage() {
+	const [user, setUser] = useState();
+
+	const { user: loggedUser } = useAuth();
+
+	useEffect(() => {
+		try {
+			async function fetchUser() {
+				const response = await api.get(`/api/users/${loggedUser.id}`);
+				const user = await response.json();
+
+				setUser(user);
+			}
+
+			fetchUser();
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}, [loggedUser.id]);
+
+	async function handleLike(userId, postId) {
+		try {
+			await api.post("/api/posts/likes", {
+				userId,
+				postId,
+			});
+
+			const updatePostsArray = (postsList) => {
+				return postsList.map((post) => {
+					if (post.id === postId) {
+						return {
+							...post,
+							likes: [...post.likes, { user_id: userId }],
+						};
+					}
+					return post;
+				});
+			};
+
+			const updatedUser = {
+				...user,
+				posts: updatePostsArray(user.posts),
+
+				followers: user.followers.map((record) => ({
+					...record,
+					following: {
+						...record.following,
+						posts: updatePostsArray(record.following.posts),
+					},
+				})),
+			};
+
+			setUser(updatedUser);
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}
+
+	async function handleDislike(userId, postId) {
+		try {
+			await api.delete("/api/posts/likes", {
+				userId,
+				postId,
+			});
+
+			const updatePostsArray = (postsList) => {
+				return postsList.map((post) => {
+					if (post.id === postId) {
+						return {
+							...post,
+							likes: post.likes.filter((like) => like.user_id !== userId),
+						};
+					}
+					return post;
+				});
+			};
+
+			const updatedUser = {
+				...user,
+				posts: updatePostsArray(user.posts),
+
+				followers: user.followers.map((record) => ({
+					...record,
+					following: {
+						...record.following,
+						posts: updatePostsArray(record.following.posts),
+					},
+				})),
+			};
+
+			setUser(updatedUser);
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}
+
+	if (!user) {
+		return (
+			<>
+				<main className={styles.mainContainer}>
+					<Hamburger />
+					<Sidebar />
+				</main>
+			</>
+		);
+	}
+
+	const myPosts = user.posts || [];
+	const followingPosts = user.followers.flatMap((record) => {
+		return record.following.posts;
+	});
+
+	const allPosts = [...myPosts, ...followingPosts];
+
+	allPosts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
 	return (
 		<>
 			<main className={styles.mainContainer}>
 				<Hamburger />
 				<Sidebar />
-				<Posts />
+				<section className={styles.posts}>
+					{allPosts.map((post) => (
+						<Post
+							key={post.id}
+							id={post.id}
+							author={post.author}
+							content={post.content}
+							postedAt={formatDistanceToNow(new Date(post.created_at), {
+								addSuffix: false,
+							})}
+							comments={post.comments}
+							likes={post.likes}
+							handleLike={handleLike}
+							handleDislike={handleDislike}
+						/>
+					))}
+				</section>
 			</main>
 		</>
 	);
