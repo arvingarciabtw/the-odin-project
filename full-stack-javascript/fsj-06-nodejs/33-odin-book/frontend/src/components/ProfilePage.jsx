5d29bd4a5368e914e326578908e7219d516190c4
Arvin Garcia <arvin@arvingarcia.com>
2025-11-22 07:00:26 +0800

    feat: display user posts in profile page

--- full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/ProfilePage.jsx
+++ full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/ProfilePage.jsx
@@ -1,8 +1,91 @@
 import styles from "../styles/ProfilePage.module.css";
 import Sidebar from "./Sidebar";
 import Hamburger from "./Hamburger";
+import Post from "./Post";
+import { useState, useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { formatDistanceToNow } from "date-fns";
+import { api } from "../utils/api";
 
 function ProfilePage() {
+	const [user, setUser] = useState(null);
+
+	const { user: loggedUser } = useAuth();
+
+	useEffect(() => {
+		try {
+			async function fetchUser() {
+				const response = await api.get(`/api/users/${loggedUser.id}`);
+				const user = await response.json();
+
+				setUser(user);
+			}
+
+			fetchUser();
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}, []);
+
+	async function handleLike(userId, postId) {
+		try {
+			await api.post("/api/posts/likes", {
+				userId,
+				postId,
+			});
+
+			const updatedUser = {
+				...user,
+				posts: user.posts.map((post) => {
+					if (post.id === postId) {
+						return {
+							...post,
+							likes: [...post.likes, { user_id: userId }],
+						};
+					}
+					return post;
+				}),
+			};
+			setUser(updatedUser);
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}
+
+	async function handleDislike(userId, postId) {
+		try {
+			await api.delete("/api/posts/likes", {
+				userId,
+				postId,
+			});
+
+			const updatedUser = {
+				...user,
+				posts: user.posts.map((post) => {
+					if (post.id === postId) {
+						return {
+							...post,
+							likes: post.likes.filter((like) => like.user_id !== userId),
+						};
+					}
+					return post;
+				}),
+			};
+			setUser(updatedUser);
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}
+
+	if (!user) {
+		return (
+			<main className={styles.mainContainer}>
+				<Hamburger />
+				<Sidebar />
+			</main>
+		);
+	}
+
 	return (
 		<main className={styles.mainContainer}>
 			<Hamburger />
@@ -12,30 +95,46 @@ function ProfilePage() {
 					<div className={styles.profilePicture}>
 						<img src="/logo_optimized.webp" alt="Profile picture" />
 					</div>
-					<h1 className={styles.fullname}>First Last</h1>
-					<p className={styles.username}>@username</p>
+					<h1 className={styles.fullname}>
+						{user.first_name} {user.last_name}
+					</h1>
+					<p className={styles.username}>@{user.username}</p>
 					<div className={styles.profileDetails}>
 						<p>
-							<span>10</span> followers
+							<span>{user.following.length}</span> followers
 						</p>
 						<p>
-							<span>10</span> following
+							<span>{user.followers.length}</span> following
 						</p>
 						<p>
-							<span>10</span> posts
+							<span>{user.posts.length}</span> posts
 						</p>
 					</div>
 				</section>
-				<MyPosts />
+				<MyPosts user={user} handleLike={handleLike} handleDislike={handleDislike} />
 			</div>
 		</main>
 	);
 }
 
-function MyPosts() {
+function MyPosts({ user, handleLike, handleDislike }) {
 	return (
 		<section className={styles.myPosts}>
-			<p>My posts here...</p>
+			{user.posts.map((post) => (
+				<Post
+					key={post.id}
+					id={post.id}
+					author={post.author}
+					content={post.content}
+					postedAt={formatDistanceToNow(new Date(post.created_at), {
+						addSuffix: false,
+					})}
+					comments={post.comments}
+					likes={post.likes}
+					handleLike={handleLike}
+					handleDislike={handleDislike}
+				/>
+			))}
 		</section>
 	);
 }
