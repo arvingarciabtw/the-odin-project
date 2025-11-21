a6b040f726bc4e3073f15c7aeb43a05adbfb0459
Arvin Garcia <arvin@arvingarcia.com>
2025-11-22 06:58:59 +0800

    feat: create post in frontend

--- full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/CreatePost.jsx
+++ full-stack-javascript/fsj-06-nodejs/33-odin-book/frontend/src/components/CreatePost.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router";
+import { api } from "../utils/api";
+
 function CreatePost() {
+	const navigate = useNavigate();
+	const { user } = useAuth();
+
+	const [content, setContent] = useState();
+
+	async function handleChange(e) {
+		setContent(e.target.value);
+	}
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			await api.post("/api/posts", {
+				content: content,
+				authorId: user.id,
+			});
+
+			navigate("/");
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	}
+
 	return (
 		<main
 			style={{
@@ -8,7 +37,7 @@ function CreatePost() {
 				alignItems: "center",
 			}}
 		>
-			<form action="POST">
+			<form onSubmit={handleSubmit}>
 				<h1>Post Content</h1>
 				<p className="description">
 					Enter your post content here, and submit when done.
@@ -21,6 +50,7 @@ function CreatePost() {
 					style={{
 						height: 200,
 					}}
+					onChange={handleChange}
 				></textarea>
 				<button type="submit">Create</button>
 			</form>
