be705eb98667b201295ced9972723866985c5978
Arvin Garcia <arvin@arvingarcia.com>
2026-02-24 17:37:09 +0800

    refactor: version 2 of the site
    This overhauls and refactors the old site to a newer version that
    follows a design template, has light/dark theme toggle, and
    responsiveness.

--- foundations/03-rock-paper-scissors/script.js
+++ foundations/03-rock-paper-scissors/script.js
@@ -1,96 +1,241 @@
-const rockButton = document.querySelector(".rock-button");
-const paperButton = document.querySelector(".paper-button");
-const scissorsButton = document.querySelector(".scissors-button");
-const buttonsContainer = document.querySelector(".buttons-container");
-const humanScoreElement = document.querySelector(".human-score .score");
-const computerScoreElement = document.querySelector(".computer-score .score");
-const humanMoveElement = document.querySelector(".human-move");
-const computerMoveElement = document.querySelector(".computer-move");
-const container = document.querySelector(".container");
-const winnerMessage = document.querySelector(".move-message");
+/*
+  --- THEME TOGGLE ---
+*/
+const root = document.querySelector("html");
+const toggler = document.querySelector(".theme-toggle");
+const toggleIcon = document.querySelector(".theme-toggle-icon");
 
-let humanScore = 0;
-let computerScore = 0;
+if (localStorage.getItem("theme") === "dark") {
+  root.classList.add("dark");
+} else {
+  root.classList.remove("dark");
+}
 
-function getComputerChoice() {
-  const randomNum = Math.random();
-  let computerChoice = "";
-
-  if (randomNum > 0 && randomNum <= 0.33) {
-    computerChoice = "rock";
-  } else if (randomNum > 0.33 && randomNum <= 0.66) {
-    computerChoice = "paper";
+toggler.addEventListener("click", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    root.classList.remove("dark");
+    localStorage.setItem("theme", "light");
   } else {
-    computerChoice = "scissors";
-  }
-
-  return computerChoice;
-}
-
-function playRound(humanChoice, computerChoice) {
-  function updateResult(winCondition) {
-    humanMoveElement.textContent = humanChoice.toUpperCase();
-    computerMoveElement.textContent = computerChoice.toUpperCase();
-
-    if (winCondition === "humanWin") {
-      humanScoreElement.textContent = ++humanScore;
-    } else if (winCondition === "computerWin") {
-      computerScoreElement.textContent = ++computerScore;
-    }
-  }
-
-  // Draw conditions
-  if (humanChoice === "rock" && computerChoice === "rock") {
-    updateResult();
-  } else if (humanChoice === "paper" && computerChoice === "paper") {
-    updateResult();
-  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    updateResult();
-  }
-
-  // Human win conditions
-  if (humanChoice === "rock" && computerChoice === "scissors") {
-    updateResult("humanWin");
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    updateResult("humanWin");
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    updateResult("humanWin");
-  }
-
-  // Computer win conditions
-  if (humanChoice === "scissors" && computerChoice === "rock") {
-    updateResult("computerWin");
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    updateResult("computerWin");
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    updateResult("computerWin");
-  }
-
-  if (humanScore === 5 || computerScore === 5) {
-    rockButton.setAttribute("disabled", "disabled");
-    paperButton.setAttribute("disabled", "disabled");
-    scissorsButton.setAttribute("disabled", "disabled");
-
-    if (humanScore === 5) {
-      winnerMessage.textContent = "Game over! You won!";
-    } else {
-      winnerMessage.textContent = "Game over! Computer won!";
-    }
-  }
-}
-
-buttonsContainer.addEventListener("click", (event) => {
-  let target = event.target;
-
-  switch (target.className) {
-    case "rock-button":
-      playRound("rock", getComputerChoice());
-      break;
-    case "paper-button":
-      playRound("paper", getComputerChoice());
-      break;
-    case "scissors-button":
-      playRound("scissors", getComputerChoice());
-      break;
+    root.classList.add("dark");
+    localStorage.setItem("theme", "dark");
   }
 });
+
+/*
+  --- MOBILE MENU ---
+*/
+const menu = document.querySelector(".menu-button");
+const closeModal = document.querySelector(".close-modal-button");
+const modal = document.querySelector(".modal");
+
+menu.addEventListener("click", () => {
+  if (modal.style.display === "grid") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "grid";
+  }
+});
+
+closeModal.addEventListener("click", () => {
+  if (modal.style.display === "grid") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "grid";
+  }
+});
+
+/*
+  --- GAME ---
+*/
+const gameStatus = document.querySelector(".game-status");
+
+const rock = document.querySelector("#rock-btn");
+const paper = document.querySelector("#paper-btn");
+const scissors = document.querySelector("#scissors-btn");
+
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+
+rock.addEventListener("click", () => {
+  const computerChoice = getComputerChoice();
+
+  const base = `Player clicked the rock button. Computer chose ${computerChoice}.`;
+  let victoryMessage;
+
+  switch (computerChoice) {
+    case "rock":
+      victoryMessage = "No one wins...";
+      break;
+    case "paper":
+      victoryMessage = "Computer wins!";
+      setTimeout(() => {
+        computerScore.textContent = Number(computerScore.textContent) + 1;
+      }, 2000);
+      break;
+    case "scissors":
+      victoryMessage = "Player wins!";
+      setTimeout(() => {
+        playerScore.textContent = Number(playerScore.textContent) + 1;
+      }, 2000);
+      break;
+    default:
+      victoryMessage = "";
+      break;
+  }
+
+  gameStatus.textContent = "Waiting for computer's move...";
+
+  rock.setAttribute("disabled", true);
+  paper.setAttribute("disabled", true);
+  scissors.setAttribute("disabled", true);
+
+  setTimeout(() => {
+    gameStatus.textContent = `${base} ${victoryMessage}`;
+
+    rock.removeAttribute("disabled");
+    paper.removeAttribute("disabled");
+    scissors.removeAttribute("disabled");
+
+    if (playerScore.textContent === "5" || computerScore.textContent === "5") {
+      rock.setAttribute("disabled", true);
+      paper.setAttribute("disabled", true);
+      scissors.setAttribute("disabled", true);
+
+      gameStatus.textContent = "Game over!";
+    }
+  }, 2000);
+});
+paper.addEventListener("click", () => {
+  const computerChoice = getComputerChoice();
+
+  const base = `Player clicked the paper button. Computer chose ${computerChoice}.`;
+  let victoryMessage;
+
+  switch (computerChoice) {
+    case "rock":
+      victoryMessage = "Player wins!";
+      setTimeout(() => {
+        playerScore.textContent = Number(playerScore.textContent) + 1;
+
+        if (playerScore.textContent === "5") {
+          rock.setAttribute("disabled", true);
+          paper.setAttribute("disabled", true);
+          scissors.setAttribute("disabled", true);
+        }
+      }, 2000);
+      break;
+    case "paper":
+      victoryMessage = "No one wins...";
+      break;
+    case "scissors":
+      victoryMessage = "Computer wins!";
+      setTimeout(() => {
+        computerScore.textContent = Number(computerScore.textContent) + 1;
+
+        if (computerScore.textContent === "5") {
+          rock.setAttribute("disabled", true);
+          paper.setAttribute("disabled", true);
+          scissors.setAttribute("disabled", true);
+        }
+      }, 2000);
+      break;
+    default:
+      victoryMessage = "";
+      break;
+  }
+
+  gameStatus.textContent = "Waiting for computer's move...";
+
+  rock.setAttribute("disabled", true);
+  paper.setAttribute("disabled", true);
+  scissors.setAttribute("disabled", true);
+
+  setTimeout(() => {
+    gameStatus.textContent = `${base} ${victoryMessage}`;
+
+    rock.removeAttribute("disabled");
+    paper.removeAttribute("disabled");
+    scissors.removeAttribute("disabled");
+
+    if (playerScore.textContent === "5" || computerScore.textContent === "5") {
+      rock.setAttribute("disabled", true);
+      paper.setAttribute("disabled", true);
+      scissors.setAttribute("disabled", true);
+
+      gameStatus.textContent = "Game over!";
+    }
+  }, 2000);
+});
+scissors.addEventListener("click", () => {
+  const computerChoice = getComputerChoice();
+
+  const base = `Player clicked the scissors button. Computer chose ${computerChoice}.`;
+  let victoryMessage;
+
+  switch (computerChoice) {
+    case "rock":
+      victoryMessage = "Computer wins!";
+      setTimeout(() => {
+        computerScore.textContent = Number(computerScore.textContent) + 1;
+
+        if (computerScore.textContent === "5") {
+          rock.setAttribute("disabled", true);
+          paper.setAttribute("disabled", true);
+          scissors.setAttribute("disabled", true);
+        }
+      }, 2000);
+      break;
+    case "paper":
+      victoryMessage = "Player wins!";
+      setTimeout(() => {
+        playerScore.textContent = Number(playerScore.textContent) + 1;
+
+        if (playerScore.textContent === "5") {
+          rock.setAttribute("disabled", true);
+          paper.setAttribute("disabled", true);
+          scissors.setAttribute("disabled", true);
+        }
+      }, 2000);
+      break;
+    case "scissors":
+      victoryMessage = "No one wins!";
+      break;
+    default:
+      victoryMessage = "";
+      break;
+  }
+
+  gameStatus.textContent = "Waiting for computer's move...";
+
+  rock.setAttribute("disabled", true);
+  paper.setAttribute("disabled", true);
+  scissors.setAttribute("disabled", true);
+
+  setTimeout(() => {
+    gameStatus.textContent = `${base} ${victoryMessage}`;
+
+    rock.removeAttribute("disabled");
+    paper.removeAttribute("disabled");
+    scissors.removeAttribute("disabled");
+
+    if (playerScore.textContent === "5" || computerScore.textContent === "5") {
+      rock.setAttribute("disabled", true);
+      paper.setAttribute("disabled", true);
+      scissors.setAttribute("disabled", true);
+
+      gameStatus.textContent = "Game over!";
+    }
+  }, 2000);
+});
+
+function getComputerChoice() {
+  const num = Math.random();
+
+  if (num <= 0.33) {
+    return "rock";
+  } else if (num <= 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
